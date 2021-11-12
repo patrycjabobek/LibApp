5c280cc32ae802a7d3ac4dc19e73f2181353bb69
Tarzanica <patrycja.bobek@microsoft.wsei.edu.pl>
2021-11-12 23:48:48 +0100

    Added convention-based route

--- LibApp/Controllers/BooksController.cs
+++ LibApp/Controllers/BooksController.cs
@@ -31,10 +31,15 @@ namespace LibApp.Controllers
             return Content($"pageIndex={pageIndex} & sortBy={sortBy}");
         }
 
-        [Route("/books/released/{year:regex(^\\d{{4}}$)}/{month:range(1,12)}")]
+        //[Route("/books/released/{year:regex(^\\d{{4}}$)}/{month:range(1,12)}")]
+        //public IActionResult ByReleaseDate(int year, int month)
+        //{
+        //    return Content(year + "/" + month);
+        //}
+
         public IActionResult ByReleaseDate(int year, int month)
         {
-            return Content(year + "/" + month);
+            return Content($"{year}/{month}");
         }
     }
 }
