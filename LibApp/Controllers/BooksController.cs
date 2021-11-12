60356242e7674a1fac97e463e676ee15166e717b
Tarzanica <patrycja.bobek@microsoft.wsei.edu.pl>
2021-11-13 00:18:50 +0100

    Added Customer and RandomBookViewModels, modified Random view to accept RandomBookViewModel type

--- LibApp/Controllers/BooksController.cs
+++ LibApp/Controllers/BooksController.cs
@@ -1,9 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using LibApp.Models;
+using LibApp.ViewModels;
 
 namespace LibApp.Controllers
 {
@@ -13,7 +12,18 @@ namespace LibApp.Controllers
         {
             var firstBook = new Book() { Name = "English dictionary" };
 
-            return View();
+            var customers = new List<Customer>
+            {
+                new Customer{Name = "Customer 1" },
+                new Customer{Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomBookViewModel
+            {
+                Book = firstBook,
+                Customers = customers
+            };
+            return View(viewModel);
         }
 
         public IActionResult Index(int? pageIndex, string sortBy)
