4b7450fb65018addf3b8b5496781ee3fa4ed20c6
Tarzanica <patrycja.bobek@microsoft.wsei.edu.pl>
2021-11-13 07:46:52 +0100

    Added MemberShipType model

---
+++ LibApp/Models/MemberShipTypecs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibApp.Models
+{
+   
+
+    public class MemberShipTypecs
+    {
+        public byte Id { get; set; }
+        public short SignUpFee { get; set; }
+        public byte DurationInMonth { get; set; }
+        public byte DiscountRate { get; set; }
+
+    }
+}
